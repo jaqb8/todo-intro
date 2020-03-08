5d77384125903c180b8837a3ebe297776030e756
jaqb8 <qba.anisz@gmail.com>
2020-03-08 17:07:23 +0100

    Updates

---
+++ precache-manifest.07591924773ab479ee516fdd66e9fe46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13c5cf77ec542167ab815881d6b5d3e",
+    "url": "/todo-intro/index.html"
+  },
+  {
+    "revision": "65aac6bdba3deeee7ed9",
+    "url": "/todo-intro/static/css/2.9d71b1d6.chunk.css"
+  },
+  {
+    "revision": "f0a8e1b2614448736041",
+    "url": "/todo-intro/static/css/main.3fa36b5d.chunk.css"
+  },
+  {
+    "revision": "65aac6bdba3deeee7ed9",
+    "url": "/todo-intro/static/js/2.f951e485.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/todo-intro/static/js/2.f951e485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a8e1b2614448736041",
+    "url": "/todo-intro/static/js/main.0db7c65a.chunk.js"
+  },
+  {
+    "revision": "220372d45be64efe34a1",
+    "url": "/todo-intro/static/js/runtime-main.62d6f422.js"
+  }
+]);
